c3c3067a72943021a3f9c2262ae56bbfd32a7482
codepalacios <chrispalacios95@gmail.com>
2022-10-03 14:19:10 -0600

    Primer avance: > Introduccion a JavaScript - Terminado

---
+++ funciones.js
@@ -0,0 +1,21 @@
+// Declarativas
+
+function miFuncion() {
+  return 3;
+}
+
+miFuncion();
+
+// Expresión
+
+var miFuncion = function (a, b) {
+  return a + b;
+};
+
+miFuncion(); // Las funciones de tipo expresion también son conocidas como funciones anonimas por que no tienen nombres
+
+// function saludar(nombre) {
+// 	console.log(`Hola ${nombre}`);
+// }
+
+// saludar('Diego');
